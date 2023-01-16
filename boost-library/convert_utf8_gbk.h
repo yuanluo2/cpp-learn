2ac9241dbabe62693854060610ef9fc0a5b99d3b
Ge Zhenyuan <49439486+yuanluo2@users.noreply.github.com>
2023-01-16 14:27:28 +0800

    Update convert_utf8_gbk.h

--- boost-library/convert_utf8_gbk.h
+++ boost-library/convert_utf8_gbk.h
@@ -1,9 +1,10 @@
 #include <boost/locale.hpp>
+#include <string>
 
-string utf8_to_gbk(const string & str){
+std::string utf8_to_gbk(const std::string& str){
     return boost::locale::conv::between(str, "GBK", "UTF-8");
 }
 
-string gbk_to_utf8(const string & str){
+std::string gbk_to_utf8(const std::string& str){
     return boost::locale::conv::between(str, "UTF-8", "GBK");
 }
